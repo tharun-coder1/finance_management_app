df7b9df41753ca0ba2c1bdec605f96b9b747c0a7
tharun-coder1 <tharunjada@gmail.com>
2024-06-13 08:18:30 -0500

    Base build

---
+++ src/context/globalContext.js
@@ -0,0 +1,117 @@
+import React, { useContext, useState } from "react"
+import axios from 'axios'
+
+
+const BASE_URL = "http://localhost:3000/api/v1/";
+
+
+const GlobalContext = React.createContext()
+
+export const GlobalProvider = ({children}) => {
+
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    const custom_sort=(a, b)=> {
+        return new Date(a.date).getTime() - new Date(b.date).getTime();
+    }
+
+    //calculate incomes
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getIncomes()
+    }
+
+    const getIncomes = async () => {
+        //const response = await axios.get(`${BASE_URL}get-incomes`)
+        const response = await axios.get('https://run.mocky.io/v3/595020e7-d477-48dd-87cf-514c4ccc9278');
+        setIncomes(response.data.sort(custom_sort))
+        console.log(response.data)
+    }
+
+    const deleteIncome = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
+        getIncomes()
+    }
+
+    const totalIncome = () => {
+        let totalIncome = 0;
+        incomes.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
+
+    //calculate incomes
+    const addExpense = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-expense`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getExpenses()
+    }
+
+    const getExpenses = async () => {
+        //const response = await axios.get(`${BASE_URL}get-expenses`)
+        const response = await axios.get('https://run.mocky.io/v3/7c4c9acf-7c2c-41e8-ae20-b9382ced9c51');
+        setExpenses(response.data.sort(custom_sort))
+        console.log(response.data)
+    }
+
+    const deleteExpense = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-expense/${id}`)
+        getExpenses()
+    }
+
+    const totalExpenses = () => {
+        let totalIncome = 0;
+        expenses.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
+
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+         history.sort(custom_sort).reverse();
+        return history.slice(0, 3)
+    }
+
+
+    return (
+        <GlobalContext.Provider value={{
+            addIncome,
+            getIncomes,
+            incomes,
+            deleteIncome,
+            expenses,
+            totalIncome,
+            addExpense,
+            getExpenses,
+            deleteExpense,
+            totalExpenses,
+            totalBalance,
+            transactionHistory,
+            error,
+            setError
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () =>{
+    return useContext(GlobalContext)
+}
